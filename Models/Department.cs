1de1659408450c15526ddc06756ee079e142f476
Ahmed Osama <116659313+AhmedOsama25-10@users.noreply.github.com>
2022-12-25 16:52:18 +0200

    Add files via upload

---
+++ Models/Department.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace APIREV.Models
+{
+    public class Department
+    {
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string MangerName { get; set; }
+        public List<Employee>  Employees { get; set; }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIREV.Models
+{
+    public class Employee
+    {
+        
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public long Phone { get; set; }
+        public double  Salary { get; set; }
+        public string Address { get; set; }
+        [ForeignKey("Department")]
+        public int DeptID { get; set; }
+        public Department Department { get; set; }
+    }
+}
