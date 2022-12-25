c54ee65d1dfa969b86f7542a3807f0937927b7ea
Ahmed Osama <116659313+AhmedOsama25-10@users.noreply.github.com>
2022-12-25 16:48:45 +0200

    Add files via upload

---
+++ ApplicationDbContext/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using APIREV.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIREV.ApplicationDbContext
+{
+    public class AppDbContext: IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext()
+        {
+
+        }
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+    }
+}
